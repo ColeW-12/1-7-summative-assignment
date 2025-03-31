08872c2a00f36302c69f1161f2904ad93a320f48
Cole Walker <339783987@GoTVDSB.ca>
2025-03-31 14:25:05 -0400

    Add project files.

---
+++ 1-7 summative assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1_7_summative_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my casino!");
+            Console.WriteLine("How to play: You will start with a balance of money that you can bet.");
+            Console.WriteLine("You type in your answer for rock, paper and scissors and the computer will generate an answer for itself.");
+            Console.WriteLine("If you win, you get more money, and if you lose, you have the option to quit or play again.");
+        }
+    }
+}
